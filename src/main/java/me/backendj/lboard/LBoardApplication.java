71454e07778e297db60d6f4a59089128871d2362
backend-j <jeeell.922@gmail.com>
2020-09-18 21:31:38 +0900

    Initial commit

---
+++ src/main/java/me/backendj/lboard/LBoardApplication.java
@@ -0,0 +1,13 @@
+package me.backendj.lboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/backendj/lboard/LBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.backendj.lboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
