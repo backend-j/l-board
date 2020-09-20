cfbff8c2abfedc491a6ca66c19f8a9139f209cf3
backend-j <jeeell.922@gmail.com>
2020-09-20 19:08:34 +0900

    feat model mapper 의존성 추가

---
+++ src/main/java/me/backendj/lboard/common/WebConfig.java
@@ -0,0 +1,19 @@
+package me.backendj.lboard.common;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(AccessLevel.PRIVATE);
+        return modelMapper;
+    }
+}
